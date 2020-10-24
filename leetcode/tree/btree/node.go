8bd051831df4b704c4d4c6768df362f82d79b85c
qiualiang <qiualiang@outlook.com>
2020-10-25 04:07:31 +0800

    add node test

--- leetcode/tree/btree/node.go
+++ leetcode/tree/btree/node.go
@@ -31,3 +31,49 @@ func createTree(root *TreeNode, nums []interface{}, len int, index int) (tree *T
 	}
 	return root
 }
+
+func preorder(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	var left, right []int
+	if root.Left != nil {
+		left = preorder(root.Left)
+	}
+	if root.Right != nil {
+		right = preorder(root.Right)
+	}
+	res := append([]int{root.Val}, left...)
+	res = append(res, right...)
+	return res
+}
+func inorder(root *TreeNode) []int {
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Val}
+	}
+	var left, right []int
+	if root.Left != nil {
+		left = Inorder(root.Left)
+	}
+	if root.Right != nil {
+		right = Inorder(root.Right)
+	}
+	res := append(left, root.Val)
+	res = append(res, right...)
+	return res
+}
+func postorder(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	var left, right []int
+	if root.Left != nil {
+		left = postorder(root.Left)
+	}
+	if root.Right != nil {
+		right = postorder(root.Right)
+	}
+	res := append(left, right...)
+	res = append(res, root.Val)
+	return res
+}
---
+++ leetcode/tree/btree/node_test.go
@@ -0,0 +1,29 @@
+package btree
+
+import (
+	"algorithm/util"
+	"testing"
+)
+
+var nums []interface{} = []interface{}{6, 2, 8, 0, 4, 7, 9, nil, nil, 3, 5}
+var pre []int = []int{6, 2, 0, 4, 3, 5, 8, 7, 9}
+var in []int = []int{0, 2, 3, 4, 5, 6, 7, 8, 9}
+var post []int = []int{0, 3, 5, 4, 2, 7, 9, 8, 6}
+var root *TreeNode = BuildTree(nums)
+
+func TestPreorder(t *testing.T) {
+	if !util.CheckSliceEq(preorder(root), pre) {
+		t.Error("pre order error :", preorder(root))
+	}
+
+}
+func TestInorder(t *testing.T) {
+	if !util.CheckSliceEq(inorder(root), in) {
+		t.Error("in order error ", inorder(root))
+	}
+}
+func TestPostorder(t *testing.T) {
+	if !util.CheckSliceEq(postorder(root), post) {
+		t.Error("post order error ", postorder(root))
+	}
+}
