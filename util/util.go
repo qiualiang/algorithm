d8848216673f816da0fc9be60aaa9331428cd5c2
qiualiang <qiualiang@outlook.com>
2020-10-24 12:38:14 +0800

    refactor sum module

---
+++ util/util.go
@@ -0,0 +1,14 @@
+package util
+
+
+func CheckSliceEq(s1, s2 []int) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}
